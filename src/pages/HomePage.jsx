3b87c5a3a611957b7bf6b27d2cfc38261de3b7fa
Yooo31 <mrts.yoan@gmail.com>
2023-09-22 19:16:19 +0200

    Add correct match on home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import GameCard from '../components/GameCard';
+import teamsData from '../data/teams.json';
+import weekGameData from '../data/gameWeek.json';
 
 const HomePage = () => {
-  const gameData = [
-    {
-      date: '24 septembre 2023',
-      team1Logo: ' https://api-agregateur-static.ffr.fr/assets/embleme/club/642.jpg',
-      team2Logo: 'https://api-agregateur-static.ffr.fr/assets/embleme/club/3169.jpg',
-      score: '13h30',
-      team: 'Équipe 2'
-    },
-    {
-      date: '24 septembre 2023',
-      team1Logo: ' https://api-agregateur-static.ffr.fr/assets/embleme/club/642.jpg',
-      team2Logo: 'https://api-agregateur-static.ffr.fr/assets/embleme/club/3169.jpg',
-      score: '15h15',
-      team: 'Équipe 1'
-    },
-  ];
+  const [gameData, setGameData] = useState([]);
+
+  useEffect(() => {
+    const weekGame = weekGameData.weekGame;
+
+    const gamesWithInfo = weekGame.map((game) => {
+      const team1Info = teamsData.teams.find((team) => team.id === game.team1);
+      const team2Info = teamsData.teams.find((team) => team.id === game.team2);
+
+      return {
+        date: game.date,
+        team1Logo: team1Info ? team1Info.img : '',
+        team2Logo: team2Info ? team2Info.img : '',
+        score1: game.score1,
+        score2: game.score2,
+      };
+    });
+
+    setGameData(gamesWithInfo);
+  }, []);
 
   return (
     <div>
