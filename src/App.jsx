83b82439d8950359c99119b5b1bc8a22952d4c6d
SeniorYoan <yoan@kipsoft.fr>
2023-09-22 12:54:47 +0200

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import './styles/App.css';
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage'; // Importez les autres pages ici (Calendar, Ranking)
-import CalendarPage from './pages/CalendarPage'; // Importez les autres pages ici (Calendar, Ranking)
-import RankingPage from './pages/RankingPage'; // Importez les autres pages ici (Calendar, Ranking)
+import HomePage from './pages/HomePage';
+import CalendarPage from './pages/CalendarPage';
+import RankingPage from './pages/RankingPage';
+import { NavBar } from './components/NavBar';
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/calendar" element={<CalendarPage />} />
-          <Route path="/ranking" element={<RankingPage />} />
-        </Routes>
+        <NavBar />
+        <div className="container mx-auto p-4">
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/calendar" element={<CalendarPage />} />
+            <Route path="/ranking" element={<RankingPage />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
