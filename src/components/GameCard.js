ed6a4684150444c8600602e8f56f423ab947f189
SeniorYoan <yoan@kipsoft.fr>
2023-09-22 10:44:33 +0200

    Add first component

---
+++ src/components/GameCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const MatchCard = ({ date, team1Logo, team2Logo, score }) => {
+  return (
+    <div className="border p-4 rounded-md shadow-md mb-4">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center">
+          <img src={team1Logo} alt="Team 1" className="w-8 h-8 mr-2" />
+          Team 1
+        </div>
+        <div>{score || 'Time TBD'}</div>
+        <div className="flex items-center">
+          Team 2
+          <img src={team2Logo} alt="Team 2" className="w-8 h-8 ml-2" />
+        </div>
+      </div>
+      <div className="text-gray-600 mt-2">{date}</div>
+    </div>
+  );
+};
+
+export default MatchCard;
