432bd038d8ceb376637e78f1800bef2513fe521e
SeniorYoan <yoan@kipsoft.fr>
2023-09-22 10:53:50 +0200

    Add copy of home to get other page

---
+++ src/pages/RankingPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import GameCard from '../components/GameCard';
+
+const HomePage = () => {
+  const gameData = [
+    {
+      date: '2023-09-25',
+      team1Logo: ' https://api-agregateur-static.ffr.fr/assets/embleme/club/642.jpg',
+      team2Logo: 'https://api-agregateur-static.ffr.fr/assets/embleme/club/1402.jpg',
+      score: '2-1',
+    },
+    {
+      date: '2023-09-26',
+      team1Logo: ' https://api-agregateur-static.ffr.fr/assets/embleme/club/642.jpg',
+      team2Logo: 'https://api-agregateur-static.ffr.fr/assets/embleme/club/1402.jpg',
+      score: '0-0',
+    },
+    // Ajoutez plus de données de matchs ici
+  ];
+
+  return (
+    <div>
+      <h1>Accueil</h1>
+      {gameData.map((game, index) => (
+        <GameCard key={index} {...game} />
+      ))}
+    </div>
+  );
+};
+
+export default HomePage;
