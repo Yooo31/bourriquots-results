dff406718dc784f86f2c7a1d56318d7d05e7bf68
SeniorYoan <yoan@kipsoft.fr>
2023-09-28 15:34:51 +0200

    Rework of home page

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,43 +1,43 @@
 import React from 'react';
 
-const GameCard = ({ date, team1Logo, team2Logo, score1, score2 }) => {
+const GameCard = ({ dataReceived }) => {
   return (
     <div>
       <div className="border p-4 rounded-md shadow-md mb-4">
         <div className="flex justify-evenly">
           <div className="">
-            <img src={team1Logo} alt="Team 1" className="mr-2" />
+            <img src={ dataReceived.teamLocal } alt="Team 1" className="mr-2" />
           </div>
 
           <div className="flex-col items-center">
-            <div className="text-gray-600 text-center text-xs">{date}</div>
+            <div className="text-gray-600 text-center text-xs">{ dataReceived.date }</div>
             <div className="text-2xl text-center font-bold">
-              {score2 !== null ? score2 : '13h30'}
+              { dataReceived.info2 }
             </div>
             <div className="text-gray-600 text-center text-xs">Équipe 2</div>
           </div>
 
           <div className="">
-            <img src={team2Logo} alt="Team 2" className="ml-2" />
+            <img src={ dataReceived.teamVisitor } alt="Team 2" className="ml-2" />
           </div>
         </div>
       </div>
       <div className="border p-4 rounded-md shadow-md mb-4">
         <div className="flex justify-evenly">
           <div className="">
-            <img src={team1Logo} alt="Team 1" className="mr-2" />
+            <img src={ dataReceived.teamLocal } alt="Team 1" className="mr-2" />
           </div>
 
           <div className="flex-col items-center">
-            <div className="text-gray-600 text-center text-xs">{date}</div>
+            <div className="text-gray-600 text-center text-xs">{ dataReceived.date }</div>
             <div className="text-2xl text-center font-bold">
-              {score1 !== null ? score1 : '15h15'}
+              { dataReceived.info1 }
             </div>
             <div className="text-gray-600 text-center text-xs">Équipe 1</div>
           </div>
 
           <div className="">
-            <img src={team2Logo} alt="Team 2" className="ml-2" />
+            <img src={ dataReceived.teamVisitor } alt="Team 2" className="ml-2" />
           </div>
         </div>
       </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,36 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import GameCard from '../components/GameCard';
-import teamsData from '../data/teams.json';
-import weekGameData from '../data/gameWeek.json';
+// import teamsData from '../data/teams.json';
+// import weekGameData from '../data/gameWeek.json';
 
 const HomePage = () => {
-  const [gameData, setGameData] = useState([]);
+  const [nextGame, setNextGame] = useState([]);
 
   useEffect(() => {
-    const weekGame = weekGameData.weekGame;
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://api-agregateur.ffr.fr/graphql?saisonId=&variables=%7B%22structureIdentifiant%22%3A%22racing-club-salvetat-plaisance%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22a2439052b18db96fc753cdda15989d2666289c1e6796145ccb2d2fa9312fdb6c%22%7D%7D'
+        );
 
-    const gamesWithInfo = weekGame.map((game) => {
-      const team1Info = teamsData.teams.find((team) => team.id === game.team1);
-      const team2Info = teamsData.teams.find((team) => team.id === game.team2);
+        const nextGame = response.data.data.structure.Competitions[0].nextrencontre;
 
-      return {
-        date: game.date,
-        team1Logo: team1Info ? team1Info.img : '',
-        team2Logo: team2Info ? team2Info.img : '',
-        score1: game.score1,
-        score2: game.score2,
-      };
-    });
+        const date = nextGame.date
+        const teamVisitor = nextGame.CompetitionEquipeVisiteuse.Structure.embleme
+        const teamLocal = nextGame.CompetitionEquipeLocale.Structure.embleme
+        const info1 = "13h30"
+        const info2 = "15h15"
+        const nextGameData =  { date, teamVisitor, teamLocal, info1, info2 };
 
-    setGameData(gamesWithInfo);
+
+        setNextGame(nextGameData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
-    <div>
-      {gameData.map((game, index) => (
-        <GameCard key={index} {...game} />
-      ))}
-    </div>
+    <GameCard nextGameData={nextGame} />
   );
 };
 
