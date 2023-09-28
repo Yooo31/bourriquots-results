25097d97ebeb654be5e79b4ec51efe0e878e9262
SeniorYoan <yoan@kipsoft.fr>
2023-09-28 12:16:56 +0200

    Change style of table

--- src/components/RankingTable.jsx
+++ src/components/RankingTable.jsx
@@ -1,54 +1,37 @@
 import React from 'react';
-import { Card, Typography } from "@material-tailwind/react";
-const TABLE_HEAD = ["Place", "Logo", "Équipe", "Points"];
+import { Table } from 'flowbite-react';
+
+const TABLE_HEAD = ["Logo", "Équipe", "Points"];
 
 const RankingTable = ({ team1 }) => {
   return (
-    <Card className="h-full w-full overflow-scroll">
-      <table className="w-full min-w-max table-auto text-left">
-        <thead>
-          <tr>
-            {TABLE_HEAD.map((head) => (
-              <th key={head} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                  className="font-normal leading-none opacity-70"
-                >
-                  {head}
-                </Typography>
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {team1.map((data1, index) => (
-            <tr key={data1.id} className="even:bg-blue-gray-50/50">
-              <td className="p-4">
-                <Typography variant="small" color="blue-gray" className="font-normal">
-                  {index + 1}
-                </Typography>
-              </td>
-              <td className="p-4">
-                <Typography variant="small" color="blue-gray" className="font-normal">
-                <img src={data1.img} alt="Team 1" className="mr-2" />
-                </Typography>
-              </td>
-              <td className="p-4">
-                <Typography variant="small" color="blue-gray" className="font-normal">
-                  {data1.name}
-                </Typography>
-              </td>
-              <td className="p-4">
-                <Typography variant="small" color="blue-gray" className="font-normal">
-                  {data1.points}
-                </Typography>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </Card>
+    <Table striped>
+
+      <Table.Head>
+        {TABLE_HEAD.map((head) => (
+          <Table.HeadCell>
+            { head }
+          </Table.HeadCell>
+        ))}
+      </Table.Head>
+
+      <Table.Body className="divide-y">
+        {team1.map((data1, index) => (
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell>
+              <img src={data1.img} alt={data1.name} />
+            </Table.Cell>
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {data1.name}
+            </Table.Cell>
+            <Table.Cell>
+              {data1.points}
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+
+    </Table>
   );
 };
 
