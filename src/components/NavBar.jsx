0d78007c2de2476858314c1beb0ca3b0506b1eed
SeniorYoan <yoan@kipsoft.fr>
2023-09-28 12:34:56 +0200

    Put navbar to dark

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
-// Définir une constante pour la liste des onglets du menu
 const menuItems = [
   { text: "Accueil", link: "/", exact: true },
   { text: "Calendrier", link: "/calendar" },
   { text: "Classement", link: "/ranking" },
-  // Ajoutez d'autres éléments de menu si nécessaire
 ];
 
 function NavBar() {
@@ -17,13 +15,13 @@ function NavBar() {
   };
 
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600 text-center relative">
+    <nav className="bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 border-gray-600 text-center relative">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
         <div className="flex md:order-2">
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
             aria-controls="navbar-sticky"
             aria-expanded={isOpen}
             onClick={toggleMenu}
@@ -52,7 +50,7 @@ function NavBar() {
           } items-center justify-between w-full md:flex md:w-auto md:order-1`}
           id="navbar-sticky"
         >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white bg-gray-800 md:bg-gray-900 border-gray-700">
             {menuItems.map((item, index) => (
               <li key={index}>
                 <NavLink
@@ -60,8 +58,8 @@ function NavBar() {
                   exact={item.exact}
                   className={`block py-2 pl-3 pr-4 rounded ${
                     isOpen
-                      ? "text-white md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                      : "text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                      ? "text-white md:bg-transparent md:text-blue-700 md:p-0 md:text-blue-500"
+                      : "text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:hover:text-blue-500 text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700"
                   }`}
                   aria-current="page"
                 >
