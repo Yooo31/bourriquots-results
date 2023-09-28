313f102d08b1ae426c14b46676a16a0c56ed2a29
SeniorYoan <yoan@kipsoft.fr>
2023-09-28 16:39:27 +0200

    Change home page with request to get info

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,43 +1,47 @@
 import React from 'react';
 
-const GameCard = ({ dataReceived }) => {
+const GameCard = ({ gameData }) => {
   return (
     <div>
+      <div className="my-10">
+        <h2 class="text-4xl font-extrabold">{ gameData.title }</h2>
+      </div>
+
       <div className="border p-4 rounded-md shadow-md mb-4">
         <div className="flex justify-evenly">
           <div className="">
-            <img src={ dataReceived.teamLocal } alt="Team 1" className="mr-2" />
+            <img src={ gameData.teamLocal } alt="Team 1" className="mr-2" />
           </div>
 
           <div className="flex-col items-center">
-            <div className="text-gray-600 text-center text-xs">{ dataReceived.date }</div>
+            <div className="text-gray-600 text-center text-xs">{ gameData.date }</div>
             <div className="text-2xl text-center font-bold">
-              { dataReceived.info2 }
+              { gameData.info2 }
             </div>
             <div className="text-gray-600 text-center text-xs">Équipe 2</div>
           </div>
 
           <div className="">
-            <img src={ dataReceived.teamVisitor } alt="Team 2" className="ml-2" />
+            <img src={ gameData.teamVisitor } alt="Team 2" className="ml-2" />
           </div>
         </div>
       </div>
       <div className="border p-4 rounded-md shadow-md mb-4">
         <div className="flex justify-evenly">
           <div className="">
-            <img src={ dataReceived.teamLocal } alt="Team 1" className="mr-2" />
+            <img src={ gameData.teamLocal } alt="Team 1" className="mr-2" />
           </div>
 
           <div className="flex-col items-center">
-            <div className="text-gray-600 text-center text-xs">{ dataReceived.date }</div>
+            <div className="text-gray-600 text-center text-xs">{ gameData.date }</div>
             <div className="text-2xl text-center font-bold">
-              { dataReceived.info1 }
+              { gameData.info1 }
             </div>
             <div className="text-gray-600 text-center text-xs">Équipe 1</div>
           </div>
 
           <div className="">
-            <img src={ dataReceived.teamVisitor } alt="Team 2" className="ml-2" />
+            <img src={ gameData.teamVisitor } alt="Team 2" className="ml-2" />
           </div>
         </div>
       </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { format } from 'date-fns';
+import { fr } from 'date-fns/locale';
+import axios from 'axios';
 import GameCard from '../components/GameCard';
 // import teamsData from '../data/teams.json';
 // import weekGameData from '../data/gameWeek.json';
 
 const HomePage = () => {
   const [nextGame, setNextGame] = useState([]);
+  const [lastGame, setLastGame] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,15 +19,31 @@ const HomePage = () => {
 
         const nextGame = response.data.data.structure.Competitions[0].nextrencontre;
 
-        const date = nextGame.date
-        const teamVisitor = nextGame.CompetitionEquipeVisiteuse.Structure.embleme
-        const teamLocal = nextGame.CompetitionEquipeLocale.Structure.embleme
-        const info1 = "13h30"
-        const info2 = "15h15"
-        const nextGameData =  { date, teamVisitor, teamLocal, info1, info2 };
+        const title = "Match suivant :";
+        const dateTime = nextGame.date;
+        const date = format(new Date(dateTime), 'dd MMMM', { locale: fr });
+        const teamVisitor = nextGame.CompetitionEquipeVisiteuse.Structure.embleme;
+        const teamLocal = nextGame.CompetitionEquipeLocale.Structure.embleme;
+        const info1 = "15h15";
+        const info2 = "13h30";
 
+        const nextGameData =  { title, date, teamVisitor, teamLocal, info1, info2 };
+
+        const lastGame = response.data.data.structure.Competitions[0].lastrencontre;
+        const lastGame2 = response.data.data.structure.Competitions[1].lastrencontre;
+
+        const lastTitle = "Match précédent :";
+        const lastDateTime = lastGame.date;
+        const lastDate = format(new Date(lastDateTime), 'dd MMMM', { locale: fr });
+        const lastTeamVisitor = lastGame.CompetitionEquipeVisiteuse.Structure.embleme;
+        const lastTeamLocal = lastGame.CompetitionEquipeLocale.Structure.embleme;
+        const lastInfo1 = lastGame.RencontreResultatLocale.pointsDeMarque + " - " + lastGame.RencontreResultatVisiteuse.pointsDeMarque;
+        const lastInfo2 = lastGame2.RencontreResultatLocale.pointsDeMarque + " - " + lastGame2.RencontreResultatVisiteuse.pointsDeMarque;
+
+        const lastGameData =  { title: lastTitle, date: lastDate, teamVisitor: lastTeamVisitor, teamLocal: lastTeamLocal, info1: lastInfo1, info2: lastInfo2 };
 
         setNextGame(nextGameData);
+        setLastGame(lastGameData);
       } catch (error) {
         console.error(error);
       }
@@ -33,7 +53,10 @@ const HomePage = () => {
   }, []);
 
   return (
-    <GameCard nextGameData={nextGame} />
+    <div>
+      <GameCard gameData={ nextGame } />
+      <GameCard gameData={ lastGame } />
+    </div>
   );
 };
 
